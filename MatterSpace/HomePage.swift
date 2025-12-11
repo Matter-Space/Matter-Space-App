4936fe6219d247695054425930a09d28597ebe63
Octavia <octaviatmucheche@gmail.com>
2025-12-11 14:37:52 +0200

    added home and community tabs

--- MatterSpace/HomePage.swift
+++ MatterSpace/HomePage.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct HomePage: View {
+    @AppStorage("userType") var userType: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List(){
+                NavigationLink("Donors"){
+                    Donors()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+                NavigationLink("Hiring Companies"){
+                    HiringCompanies()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+                NavigationLink("Managers"){
+                    Managers()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+                NavigationLink("Project Manager"){
+                    ProjectManagers()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+                NavigationLink("Facilitators"){
+                    Facilitators()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+                NavigationLink("Students"){
+                    Students()
+                }
+                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
+            
+            }
+            
+            .navigationTitle("Home")
+        }
     }
 }
 
--- MatterSpace/TabsPage.swift
+++ MatterSpace/TabsPage.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct TabsPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            HomePage()
+                .tabItem{
+                    Label("Home", systemImage: "house.fill")
+                        
+                }
+            Community()
+                .tabItem {
+                    Label("Community", systemImage: "person.3")
+                       
+                }
+                
+            }
+        
+        }
+
     }
-}
+
 
 #Preview {
     TabsPage()
