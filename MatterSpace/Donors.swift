5efb3f270dcf30f7f766b5e5da2eceee0b090406
Sean <mavangirasean@gmail.com>
2025-12-11 15:23:12 +0200

    created the list for the donors and for the hiring companies. Then made links for them to go to the actual websites.

--- MatterSpace/Donors.swift
+++ MatterSpace/Donors.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct Donors: View {
+    @Environment(\.openURL) var openURL
+    
+    @State private var donors = [
+        ("Matter", "https://www.matter.ngo"),
+        ("Love for Africa", "https://loveforafrica.net/locations/zimbabwe/")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            List {
+                ForEach(donors, id: \.0) { name, url in
+                    Text(name)
+                        .onTapGesture {
+                            if let link = URL(string: url) {
+                                openURL(link)
+                            }
+                        }
+                }
+            }
+            .navigationTitle("Donors")
+        }
     }
 }
-
 #Preview {
     Donors()
 }
