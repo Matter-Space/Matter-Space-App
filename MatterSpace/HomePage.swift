f395a645ed62989146a12d905f298a489441937a
Octavia <octaviatmucheche@gmail.com>
2025-12-11 15:11:56 +0200

    edited the list

--- MatterSpace/HomePage.swift
+++ MatterSpace/HomePage.swift
@@ -10,39 +10,47 @@ import SwiftUI
 struct HomePage: View {
     @AppStorage("userType") var userType: String = ""
     var body: some View {
+        
         NavigationView{
-            List(){
-                NavigationLink("Donors"){
-                    Donors()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
-                NavigationLink("Hiring Companies"){
-                    HiringCompanies()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
-                NavigationLink("Managers"){
-                    Managers()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
-                NavigationLink("Project Manager"){
-                    ProjectManagers()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
-                NavigationLink("Facilitators"){
-                    Facilitators()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
-                NavigationLink("Students"){
-                    Students()
-                }
-                .listRowInsets(EdgeInsets(top: 30, leading:26, bottom: 30, trailing: 26))
             
+                List(){
+                    NavigationLink("Donors"){
+                        Donors()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                    NavigationLink("Hiring Companies"){
+                        HiringCompanies()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                    NavigationLink("Managers"){
+                        Managers()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                    NavigationLink("Project Manager"){
+                        ProjectManagers()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                    
+                    NavigationLink("Facilitators"){
+                        Facilitators()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                    NavigationLink("Students"){
+                        Students()
+                    }
+                    .listRowInsets(EdgeInsets(top:40, leading:16, bottom:40, trailing:16))
+                    
+                }
+               .fontWeight(.bold)
+                .navigationTitle("Home")
             }
-            
-            .navigationTitle("Home")
         }
     }
-}
 
 #Preview {
     HomePage()
