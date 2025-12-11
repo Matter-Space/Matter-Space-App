d4706e9f350231ef541dda11b41bccce15a838f5
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-12-11 10:52:18 +0200

    Initial Commit

---
+++ MatterSpace/MatterSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatterSpaceApp.swift
+//  MatterSpace
+//
+//  Created by Benard Chiherenge on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MatterSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
